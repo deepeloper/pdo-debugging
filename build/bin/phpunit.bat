9469b56580823c6de47bf17453f1459dc7e03800
weregod <don.bidon.dev@gmail.com>
2022-12-05 13:19:15 +0700

    Useless comments removed.

--- build/bin/phpunit.bat
+++ build/bin/phpunit.bat
@@ -1,4 +1,3 @@
 @cd "../.."
 @echo ------------------------------------------------------
-@rem call php "vendor/bin/phpunit" -c "build/config/phpunit.xml" --log-junit "build/logs/junit.xml"
-@call "d:/dev/winnmp/bin/php.bat" "vendor/bin/phpunit" -c "build/config/phpunit.xml" --log-junit "build/logs/junit.xml" -v --debug
+@call php "vendor/bin/phpunit" -c "build/config/phpunit.xml" --log-junit "build/logs/junit.xml" -v --debug
